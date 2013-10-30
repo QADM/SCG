653409af882a87460aeb9e8e2499ea2e1c56ee41
Ruben Albor Cerda <trancemeister75@gmail.com>
2013-10-30 13:03:24 -0600

    Se modifica la estructura delos servicios web (15 clasif)

--- opentaps-1.5/opentaps/dataimport/src/org/opentaps/dataimport/domain/OperacionIngresoService.java
+++ opentaps-1.5/opentaps/dataimport/src/org/opentaps/dataimport/domain/OperacionIngresoService.java
@@ -35,12 +35,21 @@ public class OperacionIngresoService extends DomainService  {
         String idPago= (String) context.get("idPago");
         String idProductoD= (String) context.get("idProductoD");
         String idProductoH= (String) context.get("idProductoH");
-        String ciclo= (String) context.get("ciclo");
-        String ue_S= (String) context.get("ue");
-        String n5_S= (String) context.get("n5");
-        String sfe_S= (String) context.get("sfe");
-        String loc_S= (String) context.get("loc");
-        String concatenacion= (String) context.get("concatenacion");
+        String clasif1= (String) context.get("clasif1");
+        String clasif2 = (String) context.get("clasif2");
+        String clasif3 = (String) context.get("clasif3");
+        String clasif4 = (String) context.get("clasif4");
+        String clasif5 = (String) context.get("clasif5");
+        String clasif6 = (String) context.get("clasif6");
+        String clasif7 = (String) context.get("clasif7");
+        String clasif8 = (String) context.get("clasif8");
+        String clasif9 = (String) context.get("clasif9");
+        String clasif10 = (String) context.get("clasif10");
+        String clasif11 = (String) context.get("clasif11");
+        String clasif12 = (String) context.get("clasif12");
+        String clasif13 = (String) context.get("clasif13");
+        String clasif14 = (String) context.get("clasif14");
+        String clasif15 = (String) context.get("clasif15");
         
         //Crea un registro de la entidad
         GenericValue dataImportIngresoDiario = GenericValue.create(del.getModelEntity("DataImportIngresoDiario"));
@@ -55,12 +64,22 @@ public class OperacionIngresoService extends DomainService  {
         dataImportIngresoDiario.set("idPago", idPago );
         dataImportIngresoDiario.set("idProductoD", idProductoD );
         dataImportIngresoDiario.set("idProductoH", idProductoH );
-        dataImportIngresoDiario.set("ciclo", ciclo);
-        dataImportIngresoDiario.set("ue", ue_S );
-        dataImportIngresoDiario.set("n5", n5_S);
-        dataImportIngresoDiario.set("sfe", sfe_S);
-        dataImportIngresoDiario.set("loc", loc_S );
-        dataImportIngresoDiario.set("clavePres", concatenacion);
+        dataImportIngresoDiario.set("clasificacion1", clasif1);
+        dataImportIngresoDiario.set("clasificacion2", clasif2 );
+        dataImportIngresoDiario.set("clasificacion3", clasif3);
+        dataImportIngresoDiario.set("clasificacion4", clasif4);
+        dataImportIngresoDiario.set("clasificacion5", clasif5 );
+        dataImportIngresoDiario.set("clasificacion6", clasif6 );
+        dataImportIngresoDiario.set("clasificacion7", clasif7 );
+        dataImportIngresoDiario.set("clasificacion8", clasif8 );
+        dataImportIngresoDiario.set("clasificacion9", clasif9 );
+        dataImportIngresoDiario.set("clasificacion10", clasif10 );
+        dataImportIngresoDiario.set("clasificacion11", clasif11 );
+        dataImportIngresoDiario.set("clasificacion12", clasif12 );
+        dataImportIngresoDiario.set("clasificacion13", clasif13 );
+        dataImportIngresoDiario.set("clasificacion14", clasif14 );
+        dataImportIngresoDiario.set("clasificacion15", clasif15 );
+        //dataImportIngresoDiario.set("clavePres", concatenacion);
         dataImportIngresoDiario.create();
         
         
