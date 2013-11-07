20694f3de4818e6e8f33ea0ac660b1c137f3fdea
Ruben Albor Cerda <trancemeister75@gmail.com>
2013-11-07 13:03:42 -0600

    Actualizacion Contenedor

--- opentaps-1.5/opentaps/dataimport/src/org/opentaps/dataimport/domain/ContenedorContable.java
+++ opentaps-1.5/opentaps/dataimport/src/org/opentaps/dataimport/domain/ContenedorContable.java
@@ -1,37 +1,32 @@
 package org.opentaps.dataimport.domain;
 
+import java.util.List;
+
+import org.opentaps.base.entities.Enumeration;
+import org.opentaps.base.entities.Geo;
+import org.opentaps.base.entities.Party;
+import org.opentaps.base.entities.ProductCategory;
+import org.opentaps.base.entities.WorkEffort;
+
 public class ContenedorContable {
-	private String cri;
-	private String cog;
-	private String tipoGasto;
 	private String mensaje;
+	private Party party;
+	private Geo geo;
+	private WorkEffort we;
+	private ProductCategory product;
+	private List<Enumeration> enumeration;
+	private String clavePresupuestal;
 	
 	public ContenedorContable(){
 		
 	}
-
-	public String getCri() {
-		return cri;
+	
+	public String getClavePresupuestal() {
+		return clavePresupuestal;
 	}
-
-	public void setCri(String cri) {
-		this.cri = cri;
-	}
-
-	public String getCog() {
-		return cog;
-	}
-
-	public void setCog(String cog) {
-		this.cog = cog;
-	}
-
-	public String getTipoGasto() {
-		return tipoGasto;
-	}
-
-	public void setTipoGasto(String tipoGasto) {
-		this.tipoGasto = tipoGasto;
+	
+	public void setClavePresupuestal(String clavePresupuestal) {
+		this.clavePresupuestal = clavePresupuestal;
 	}
 	
 	public String getMensaje() {
@@ -41,5 +36,44 @@ public class ContenedorContable {
 	public void setMensaje(String mensaje) {
 		this.mensaje = mensaje;
 	}
-
+	
+	public List<Enumeration> getEnumeration() {
+		return enumeration;
+	}
+	
+	public Geo getGeo() {
+		return geo;
+	}
+	
+	public Party getParty() {
+		return party;
+	}
+	
+	public ProductCategory getProduct() {
+		return product;
+	}
+	
+	public WorkEffort getWe() {
+		return we;
+	}
+	
+	public void setEnumeration(List<Enumeration> enumeration) {
+		this.enumeration = enumeration;
+	}
+	
+	public void setGeo(Geo geo) {
+		this.geo = geo;
+	}
+	
+	public void setParty(Party party) {
+		this.party = party;
+	}
+	
+	public void setProduct(ProductCategory product) {
+		this.product = product;
+	}
+	
+	public void setWe(WorkEffort we) {
+		this.we = we;
+	}
 }
